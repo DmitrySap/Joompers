999c4eb451aa7e1ce1b328f62e550561848e7fb9
muxomor <dmitrsap@mail.ru>
2023-05-11 14:53:34 +0300

    1

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,5 @@
 class BasePage:
-    def __init__(self, browser, url, timeout=4):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
